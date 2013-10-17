ffc4d0e5be20623a50f5e60a4ef9f6992aba5e35
poettlern <nikolaus.poettler@gmail.com>
2013-10-17 20:11:07 +0200

    New Network Stuff
    Player now supports more Networkvies (but not OnSerializeNetworkView!)
    
    works like a charm.. has some hiccups :D

---
+++ JnR CDm RPG/Assets/Scripts/Network/Utility/InputDispatcher.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputDispatcher : MonoBehaviour {
+	public Transform _gameManagementObject;
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Fire1"))
+		{
+			_gameManagementObject.networkView.RPC ("RemoteAttack",RPCMode.Server,
+				_gameManagementObject.GetComponent<LocalPlayer>()._networkPlayer,
+				1);
+		}
+	}
+}
--- JnR CDm RPG/Assets/Scripts/Network/Utility/LocalPlayer.cs
+++ JnR CDm RPG/Assets/Scripts/Network/Utility/LocalPlayer.cs
@@ -1,39 +1,52 @@
 using UnityEngine;
 using System.Collections;
 
+//Will only be used on the client side
 public class LocalPlayer : MonoBehaviour {
-	public NetworkPlayer _localPlayer;
-	public NetworkViewID _localTransformViewID;
+	public NetworkPlayer _networkPlayer;
+	public NetworkViewID _networkViewID;
+	public Transform _playerPrefab;
 	public bool _isInstantiated;
 	
 	[RPC]
-	private void InitPlayer(NetworkPlayer player, NetworkViewID viewID)
+	private void InitiateLocalPlayer(NetworkPlayer player, NetworkViewID viewID)
 	{
 		string text = string.Empty;
 		if (Network.isServer)
 		{
-			text += "Server: ";
+			text += "Server:";
 		}
 		else
 		{
-			text += "Client: ";
+			text += "Client:";
 		}
-		text += "Received player init " + player +". ViewIDs " + viewID;
+		text += "InitiateLocalPlayer: player init " + player +". ViewIDs " + viewID;
 		Debug.Log(text);
-		this._localPlayer = player;
-		this._localTransformViewID = viewID;
+		_networkPlayer = player;
+		_networkViewID = viewID;
 	}
+	
 	private void OnGUI()
 	{
-		GUILayout.Label("NetworkPlayer:" + this._localPlayer.guid, new GUILayoutOption[0]);
-		GUILayout.Label("NetworkViewID:" + this._localTransformViewID, new GUILayoutOption[0]);
+		if(_isInstantiated == false)
+		{
+			return;	
+		}
+		if(Network.isServer)
+		{
+			enabled = false;
+			return;	
+		}
+		GUILayout.Label("NetworkPlayer:" + this._networkPlayer.guid, new GUILayoutOption[0]);
+		GUILayout.Label("NetworkViewID:" + this._networkViewID, new GUILayoutOption[0]);
 		if (this._isInstantiated)
 		{
-			GUILayout.Label("Instantiated", new GUILayoutOption[0]);
+			GUILayout.Label("Is instantiated!", new GUILayoutOption[0]);
 		}
 		else
 		{
-			GUILayout.Label("Not Instantiated", new GUILayoutOption[0]);
+			GUILayout.Label("Is not instantiated", new GUILayoutOption[0]);
 		}
+		GUILayout.Label ("HP: " + _playerPrefab.GetComponent<PlayerState>()._hp, new GUILayoutOption[0]);
 	}
 }

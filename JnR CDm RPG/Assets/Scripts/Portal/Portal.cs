1365f3f548971867cab522f07d13369a1e07e4cf
CKovacs <christian.kovacs89@gmail.com>
2013-10-18 15:47:49 +0200

    Portal

---
+++ JnR CDm RPG/Assets/Scripts/Portal/Portal.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Portal : MonoBehaviour 
+{
+    public GameObject _linkedPortal;
+
+    public void OnTriggerEnter(Collider collider) 
+    {
+        collider.gameObject.transform.position = _linkedPortal.transform.position + collider.gameObject.transform.forward;
+    }
+}

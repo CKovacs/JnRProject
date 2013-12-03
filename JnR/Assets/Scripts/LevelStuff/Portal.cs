6f76df5b79e5dcb368babbb024864527e7ddae84
poettlr <nikolaus.poettler@gmail.com>
2013-12-03 16:49:15 +0100

    Better Portal Handling No more Errors in GUI

--- JnR/Assets/Scripts/LevelStuff/Portal.cs
+++ JnR/Assets/Scripts/LevelStuff/Portal.cs
@@ -1,14 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+
 
 public class Portal : MonoBehaviour 
 {
     public GameObject _linkedPortal;
+    public GameManager _gameManager;
+    public float _coolDownPortalUse = 3;
 
     public void OnTriggerEnter(Collider collider) 
 	{
-		Vector3 tmp = _linkedPortal.transform.position;
-		tmp.y += 1;
-        collider.gameObject.transform.position = tmp + collider.gameObject.transform.forward *2;
+        Debug.Log("TEST");
+        if(Network.isServer)
+        {
+
+            //Gather Player Information
+            Transform player = collider.gameObject.transform;
+            PlayerState playerState = player.GetComponent<PlayerState>();
+            NetworkPlayer networkPlayer = playerState._networkPlayer;
+            float ct = Time.time;
+            //Check if player is allowed to enter the portal
+            if(playerState._lastPortalTime <= ct -3f)
+            {
+                playerState._lastPortalTime = ct;
+                _gameManager.networkView.RPC("PlayerReEnabling", RPCMode.Others, networkPlayer, false ? 1 : 0);
+		        Vector3 tmp = _linkedPortal.transform.position;
+		        tmp.y += 1;
+                tmp = tmp + collider.gameObject.transform.forward *2;
+                _gameManager.networkView.RPC("SetRespawnPosition", RPCMode.Others, networkPlayer, tmp);
+                player.position = tmp;
+                _gameManager.networkView.RPC("PlayerReEnabling", RPCMode.Others, networkPlayer, true ? 1 : 0);
+            }
+        }
     }
 }
